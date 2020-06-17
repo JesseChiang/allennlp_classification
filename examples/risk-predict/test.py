ec3ba61ea53ac32d681b0a01f5706d832b296ffe
JesseChiang <117020119@link.cuhk.edu.cn>
2020-06-17 18:52:37 +0800

    updated

--- examples/risk-predict/test.py
+++ examples/risk-predict/test.py
@@ -2,6 +2,7 @@ import requests
 import csv
 import logging
 
+'''
 logging.basicConfig(format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
                     datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
@@ -23,10 +24,9 @@ with open("C:\\Users\\jesse\\Documents\\Datago\\data\\I6.csv", 'r') as f:
                          'prob': r.json()['prob']})
 
 predict.close()
-
-
 '''
-data = {'sentence': "极米被诉侵权产品 Z6 系列型号投影仪包括Z6和Z6X被诉侵权产品所采用的技术方案构成侵权。"}
+
+data = {'title': '极米被诉侵权产品 Z6 系列型号投影仪',
+        'content': "酒鬼酒称，本公司严禁在产品中添加甜蜜素，本公司已经提请相关市场监管部门对本公司市场流通产品进行全面检测，并第一时间向社会公布检测结果。\n所采用的技术方案构成侵权。"}
 r = requests.post("http://localhost:8001/predict", data=data)
-print(r.json()['label'])
-'''
+print(r.json())
