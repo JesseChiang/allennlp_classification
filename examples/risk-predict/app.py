faaff4aff9b7c64194016626adfbd5055d89965a
JesseChiang <117020119@link.cuhk.edu.cn>
2020-06-18 12:03:17 +0800

    add a model for i 4

--- examples/risk-predict/app.py
+++ examples/risk-predict/app.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     predictor = RiskPredictor(path)
     predictor.add_predictor('企业经营相关', '~/workplace/news_risk/model/i3/model.tar.gz')
     predictor.add_predictor('产品质量相关', '~/workplace/news_risk/model/i6/model.tar.gz')
+    predictor.add_predictor('企业管理相关', '~/workplace/news_risk/model/i4/model.tar.gz')
 
     logging.info(f'创建app')
     app = make_app(predictor)
